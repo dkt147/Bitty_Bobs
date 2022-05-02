a8911ce8980643663b6bf34bf0be4d3d4f8f35be
Daniyal khan <dk720820@gmail.com>
2022-05-02 03:08:05 -0700

    Completed

--- Admin/updateabout.php
+++ Admin/updateabout.php
@@ -4,25 +4,24 @@
 //Checking if button is clicked or not...
 if (isset($_POST['Update'])) {
 
-
+include 'connection.php';
 //Getting data from Form...
-    echo $d1 = $_POST['d1'];
-    echo $d2 = $_POST['d2'];
-    echo $d3 = $_POST['d3'];
-    echo $eid = $_POST['id'];
+    $d1 = $_POST['d1'];
+    $d2 = $_POST['d2'];
+    $d3 = $_POST['d3'];
+    $eid = $_POST['id'];
 
-       //Stablishing Connection...
-       include 'connection.php';    
-//Update Query For Mysql...
-        echo $query = "UPDATE `about` SET `detail1`='$d1',`detail2`='$d2',`detail3`='$d3' WHERE id = $eid";
-        $res = mysqli_query($con, $query);
-
-        if($res){
-//Redirection to another page...
-header("Location: edit_about.php");
-        }
+    $query1 = $con->prepare("UPDATE about SET detail1=?, detail2=?,detail3=? WHERE id = ?");
+        $query1->bind_param('sssi',$d1, $d2,$d3,$eid); 
+        $query1->execute();
+        $query1->close();
+    
+    
+    header("Location: edit_about.php");
 
 //Connection Close...
         mysqli_close($con);
    
 }
+
+?>
--- Admin/updateheader.php
+++ Admin/updateheader.php
@@ -4,24 +4,23 @@
 //Checking if button is clicked or not...
 if (isset($_POST['Update'])) {
 
-
+include 'connection.php';
 //Getting data from Form...
-    echo $name = $_POST['h1'];
-    echo $pass = $_POST['h2'];
-    echo $eid = $_POST['id'];
+echo $name = $_POST['h1'];
+echo $pass = $_POST['h2'];
+echo $eid = $_POST['id'];
 
-       //Stablishing Connection...
-       include 'connection.php';    
-//Update Query For Mysql...
-        echo $query = "UPDATE `header` SET `h1`='$name',`h2`='$pass' WHERE id = $eid";
-        $res = mysqli_query($con, $query);
-
-        if($res){
-//Redirection to another page...
-header("Location: edit_header.php");
-        }
-
+        $query1 = $con->prepare("UPDATE header SET h1=?, h2=? WHERE id = ?");
+        $query1->bind_param('ssi',$name, $pass,$eid); 
+        $query1->execute();
+        $query1->close();
+    
+    
+    header("Location: edit_header.php");
+    
 //Connection Close...
         mysqli_close($con);
    
 }
+
+?>
